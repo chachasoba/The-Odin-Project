099771452ef3eee5121ac5c936db5723791b5c3e
Yip Di Hang <dihang94@gmail.com>
2023-08-22 18:31:12 +0800

    Completed exercises 10 and 11

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  position = parseInt(position);
+  if (Math.sign(position) === -1) return 'OOPS';
+  if (position === 0) return 0;
+  if (position === 1) return 1;
+  else {
+    num = fibonacci(position - 1) + fibonacci(position - 2);
+  }
+  return num;
 
 };
 
