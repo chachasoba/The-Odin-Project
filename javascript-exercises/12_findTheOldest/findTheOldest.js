4ce5ac158c6f29fdbcb920f298f9761a255035a8
Yip Di Hang <dihang94@gmail.com>
2023-08-22 19:30:43 +0800

    Completed exercise 12

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let oldestAge = 0;
+  let oldestPerson = undefined;
 
+  for (const person of array) {
+    if (person.yearOfDeath == undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+
+    let age = person.yearOfDeath - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    };
+  }
+  return oldestPerson;
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
