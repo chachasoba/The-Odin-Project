4915952f837d259ce5618394ef61c3e73c2a0d45
YDH <dihang94@gmail.com>
2023-08-25 15:08:03 +0800

    Fix grid unable to fill up container

--- Etch-a-Sketch/javascript.js
+++ Etch-a-Sketch/javascript.js
@@ -1,25 +1,50 @@
 //part 1
 const grid = document.querySelector('.grid');
-const GRID_SIZE = 16
+let gridSize = 10;
 
-for (let i=0; i<GRID_SIZE; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    //column.textContent = `column${i}`
-
-    for (let i=0; i<GRID_SIZE; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        //row.textContent = `row${i}`
-
-        column.appendChild(row);
-    }
-    grid.appendChild(column);
+function removeGrid() {
+    columns = document.querySelectorAll('.column');
+    columns.forEach(column => grid.removeChild(column)); 
 }
 
+function createGrid () {
+    for (let i=0; i<gridSize; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        //column.textContent = `column${i}`
+    
+        for (let i=0; i<gridSize; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            //row.textContent = `row${i}`
+            
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }
+}
+
+const button = document.querySelector('.grid-size');
+button.addEventListener('click', () => 
+    {   
+        let userInput = prompt('Enter grid size');
+        gridSize = parseInt(userInput); 
+        removeGrid();
+        createGrid();
+        sketchGrid();
+    }
+);
+
+
 //part 2
-const rows = document.querySelectorAll('.row');
-rows.forEach(row => 
-    row.addEventListener('mouseover', () =>
-        row.classList.toggle('hover')));
+function sketchGrid(){
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => 
+        row.addEventListener('mouseover', () =>
+            row.classList.add('hover')
+        )
+    );
+}
+
+
 
